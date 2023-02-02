a01f024d8bbcad1b9c3d6b67b596ee97757398ac
Xavier Rowley <xavierrowley@protonmail.com>
2023-02-02 08:08:11 +0000

    Added Challenge .py files

---
+++ challenge_7.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+color_sensor = ColorSensor(Port.S2)
+ultrasnoic_sensor = UltrasonicSensor(Port.S3)
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.A)
+
+# Initialise DriveBase
+harvier = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Write your program here.
+while True:
+    harvier.drive(60, 0)
+    if color_sensor.color() == None:
+        harvier.straight(-120)
+        harvier.turn(120)
+    if ultrasnoic_sensor.distance() < 250:
+        harvier.turn(120)
